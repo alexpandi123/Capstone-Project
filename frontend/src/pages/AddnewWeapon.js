ba412606027ac4ef993a60a45fc32035522460b2
Alex Pandi <ppandalex@hotmail.com>
2023-07-25 00:40:27 +0100

    added functionality to "add a new weapon" page

--- frontend/src/pages/AddnewWeapon.js
+++ frontend/src/pages/AddnewWeapon.js
@@ -4,62 +4,55 @@ import Form from 'react-bootstrap/Form';
 import { Button } from "react-bootstrap";
 import { useState } from "react";
 
-const AddNewWeaponPage = () => {
-    
-    // const formElements = document.getElementById('add-new-weapon-form');
-    // formElements.addEventListener('submit', (event) => {
-    //     event.preventDefault();
+const AddNewWeaponPage = (props) => {
+    const [formData, setformData] = useState(props.formData);
 
-    //     const formData = new FormData(formElements);
-    //     formData.get('name');
-    //     console.log(formData);
-    // });
 
-    const formDataSending = () => {
-        const formData = new FormData(document.getElementById('add-new-weapon-form'));
-        
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
         fetch('http://localhost:3005/weapons/add-weapon', {
-            method: "POST",
-            body: formData
-        });
-        console.log('fetched');
+                    method: 'POST',
+                    body: JSON.stringify(formData),
+                    headers: {'Content-Type': 'application/json'}
+                })
+        event.target.reset();
     };
-
 
     return(
         <>
             <h4 className="m-auto mt-4 mb-4 d-block w-50 text-center border-bottom border-info rounded-bottom">
                 Please enter the details of the new weapon below:
             </h4>
-                <Form id="add-new-weapon-form" className="d-flex flex-column align-items-center justify-content-center">
+                <Form onSubmit={handleSubmit} id="add-new-weapon-form" className="d-flex flex-column align-items-center justify-content-center">
                         <FloatingLabel label="Name" className="mb-2 col-sm-3">
-                            <Form.Control controlId="floatingName" />
+                            <Form.Control name="name" onChange={(event) => setformData({...formData, name: event.target.value})} type="string" placeholder="name" />
                         </FloatingLabel>
-                        <FloatingLabel controlId="floatingCountry" label="Country Of Origin" className="mb-2 col-sm-3">
-                            <Form.Control type="string" placeholder="Password" />
+                        <FloatingLabel label="Country Of Origin" className="mb-2 col-sm-3">
+                            <Form.Control name="countryOfOrigin" onChange={(event) => setformData({...formData, countryOfOrigin: event.target.value})} type="string" placeholder="countryoforigin"  />
                         </FloatingLabel>
-                        <FloatingLabel controlId="floatingManYear" label="Manufacturing Year" className="mb-2 col-sm-3">
-                            <Form.Control type="string" placeholder="Password" />
+                        <FloatingLabel label="Manufacturing Year" className="mb-2 col-sm-3">
+                            <Form.Control name="designYear" onChange={(event) => setformData({...formData, designYear: event.target.value})} type="string" placeholder="designyear"  />
                         </FloatingLabel>
-                        <FloatingLabel controlId="floatingEFR" label="Effective Firing Range" className="mb-2 col-sm-3">
-                            <Form.Control type="string" placeholder="Password" />
+                        <FloatingLabel label="Effective Firing Range" className="mb-2 col-sm-3">
+                            <Form.Control name="effectiveFiringRange" onChange={(event) => setformData({...formData, effectiveFiringRange: event.target.value})} type="string" placeholder="effectiveFiringRange"  />
                         </FloatingLabel>
-                        <FloatingLabel controlId="floatingROF" label="Rate Of Fire" className="mb-2 col-sm-3">
-                            <Form.Control type="string" placeholder="Password" />
+                        <FloatingLabel label="Rate Of Fire" className="mb-2 col-sm-3">
+                            <Form.Control name="rateOfFire" onChange={(event) => setformData({...formData, rateOfFire: event.target.value})} type="string"  placeholder="rateOfFire" />
                         </FloatingLabel>
-                        <FloatingLabel controlId="floatingType" label="Type" className="mb-2 col-sm-3">
-                            <Form.Control type="string" placeholder="Password" />
+                        <FloatingLabel label="Type" className="mb-2 col-sm-3">
+                            <Form.Control name="type" onChange={(event) => setformData({...formData, type: event.target.value})} type="string"  placeholder="type" />
                         </FloatingLabel>
-                        <FloatingLabel controlId="floatingImage" label="Image" className="mb-2 col-sm-3">
-                            <Form.Control type="URL" placeholder="Image" className=""/>
+                        <FloatingLabel label="Image" className="mb-2 col-sm-3">
+                            <Form.Control name="image" onChange={(event) => setformData({...formData, image: event.target.value})} type="string" placeholder="image" /> 
                         </FloatingLabel>
-                        <FloatingLabel controlId="floatingDesc" label="Description" className="mb-2 col-sm-3">
-                            <Form.Control style={{ height: '100px' }} type="string" placeholder="Password" />
+                        <FloatingLabel label="Description" className="mb-2 col-sm-3">
+                            <Form.Control name="description" onChange={(event) => setformData({...formData, description: event.target.value})} style={{ height: '100px' }} type="string" placeholder="description"  />
                         </FloatingLabel>
-                        <FloatingLabel controlId="floatingPath" label="Path" className="mb-2 col-sm-3">
-                            <Form.Control type="string" placeholder="Password" />
+                        <FloatingLabel label="Path" className="mb-2 col-sm-3">
+                            <Form.Control name="path" onChange={(event) => setformData({...formData, path: event.target.value})} type="string"  placeholder="path" />
                         </FloatingLabel>
-                        <Button className="mt-3" onClick={() => formDataSending()} variant="outline-danger">Submit</Button>
+                        <Button className="mt-3" type="submit" variant="outline-danger">Submit</Button>
                 </Form>
         </>
     );
