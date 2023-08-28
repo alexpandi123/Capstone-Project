5a19b652e7ad4c91e6356741bf170a081c1924f7
Alex Pandi <ppandalex@hotmail.com>
2023-08-28 23:51:35 +0100

    readded frontend files as they were deleted when edited last time

---
+++ Frontend/src/App.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import SideBarMenu from "./pages/main/Sidebar";
+
+
+function App() {
+
+  return (
+    <>
+          <div>
+            <SideBarMenu />
+          </div>
+  </>
+  );
+
+};
+
+export default App;
